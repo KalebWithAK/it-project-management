07097aa5a0ef6acf32cf4ed301b2b35922812993
KalebWithAK <kad9603@gmail.com>
2023-04-19 08:09:23 -0400

    fixing dark theme and search on article pages

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-const url = "json/articles.json"
+
 let articles = []
 
 
 const all_articles = document.querySelector("div.all-articles")
 const homepage = document.querySelector("span.homepage-flag") // only add cards on homepage
+
+const url = homepage ? "json/articles.json" : "../json/articles.json"
 
 const form = document.querySelector("form")
 const search_input = document.querySelector("input.search")
@@ -14,18 +16,22 @@ button_close_search.classList.add('hidden')
 
 //DARK MODE
 const toggleSwitch = document.getElementById("toggle-switch");
-alert(toggleSwitch);
-/*const divs = document.querySelector('div');
+
+const divs = document.querySelector('div');
 
 toggleSwitch.addEventListener("change", () => {
   if (toggleSwitch.checked) {
     document.body.className = 'dark';
-    document.querySelector('.article-card').classList.add('dark');
+    if (homepage) {
+      document.querySelector('.article-card').classList.add('dark');
+    }
   } else {
     document.body.className = '';
-    document.querySelector('.article-card').classList.remove('dark');
+    if (homepage) {
+      document.querySelector('.article-card').classList.remove('dark');
+    }
   }
-});*/
+});
 
 form.onsubmit = (e) => {
   e.preventDefault() // don't reload page
@@ -40,9 +46,8 @@ fetch(url)
   .then(data => {
     articles = data
 
-    all_articles.innerHTML = ''
-
     if (homepage) {
+      all_articles.innerHTML = ''
       articles.forEach(article => {
         appendCard(article, all_articles)
       })
@@ -59,7 +64,7 @@ function appendCard(article, container) {
   else {
     card.innerHTML = `
       <a href="${article.url}">
-        <img src="./images/${article.img}">
+        <img src="${homepage ? './' : '../'}images/${article.img}">
         <h3>${article.title}</h3>
         <p class="author">By: ${article.author}</p>
       </a>
@@ -83,14 +88,14 @@ function searchArticles(search_string, articles) {
   }
 
   // display results or no results found
-  const h2 = document.querySelector("h2.search-results")
+  const h3 = document.querySelector("h3.search-results")
   const container = document.querySelector("div.search-results")
   container.innerHTML = ''
 
+  button_close_search.classList.remove('hidden')
   if (results.length) {
-    h2.textContent = `Articles related to "${search_string}"`
+    h3.textContent = `Articles related to "${search_string}"`
     results.forEach(article => appendCard(article, container))
-    button_close_search.classList.remove('hidden')
   }
   else {
     h3.textContent = `No articles found related to "${search_string}". Please check your spelling and try again.`
@@ -101,9 +106,9 @@ function closeSearchResults(e) {
   e.preventDefault()
   button_close_search.classList.add('hidden')
 
-  const h2 = document.querySelector("h2.search-results")
+  const h3 = document.querySelector("h3.search-results")
   const container = document.querySelector("div.search-results")
 
-  h2.textContent = ''
+  h3.textContent = ''
   container.innerHTML = ''
 }
